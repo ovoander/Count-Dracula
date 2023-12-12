033dde2e302fd728ee2f96fa6eb2b6988648c03f
ovoander <trounganderwatt@outlook.com>
2023-12-11 16:59:09 -0800

    refactor

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const countElement = document.getElementById("count");
 
-let count = 0;
+let count = localStorage.getItem("count") || 0;
+countElement.textContent = count;
 
 const addCount = () => {
     count++;
     countElement.textContent = count;
+    localStorage.setItem("count", count);
 };
 
 const resetCount = () => {
     count = 0;
     countElement.textContent = count;
+    localStorage.setItem("count", count);
 };
